5f0147907d8a71454beff9b18f1f4c48b8bbb783
Ashim Rudra Paul <codewithashim@gmail.com>
2023-09-02 00:58:13 +0600

    updated

--- src/Shared/Navbar/TopNav/TopNav.jsx
+++ src/Shared/Navbar/TopNav/TopNav.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
+import "./TopNav.scss";
 
 const TopNav = () => {
-    return (
-        <nav>
-            <h1>Top Nav</h1>
-        </nav>
-    );
+  return (
+    <nav>
+      <h1>Top Nav</h1>
+    </nav>
+  );
 };
 
 export default TopNav;
